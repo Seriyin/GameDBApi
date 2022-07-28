671cb96bc48e8af25a04b43f0c1a24a682b0ad98
André Diogo <redan.blue27@gmail.com>
2022-07-28 01:04:02 +0100

    Uses spread operator filter to match UTF-16.
    Prettifies code.

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ interface AltName {
     name : string
 }
 
-interface LoopControl {
+interface ILoopControl {
     credentials: ClientCredentials,
     auth_info: AuthTokenData,
     start_time: number,
@@ -35,63 +35,55 @@ interface LoopControl {
     shouldContinue: boolean,
     rate: number,
     offset: number,
-    loop_fn: (loopControl: LoopControl) => Promise<void>
 }
 
+class LoopControl implements ILoopControl {
+    credentials: ClientCredentials
+    auth_info: AuthTokenData;
+    start_time: number;
+    loop_time: number;
+    accumulate: string[];
+    shouldContinue: boolean;
+    rate: number;
+    offset: number;
+    
+    constructor(credentials: ClientCredentials, auth_info: AuthTokenData) {
+        this.credentials = credentials
+        this.auth_info = auth_info
+        const start = Date.now()
+        this.start_time = start
+        this.loop_time = start
+        this.accumulate = []
+        this.shouldContinue = true
+        this.rate = Number.parseInt(process.env.API_RATE) ?? 10
+        this.offset = 0
+    }
 
-const credentials: ClientCredentials = {
-    client_id: process.env.CLIENT_ID ?? '',
-    client_secret: process.env.CLIENT_SECRET ?? ''
-}
-
-const rate: number = Number.parseInt(process.env.API_RATE) ?? 10
-
-
-const auth_response = await fireAuthRequest(credentials)
-
-if (!checkAuthResponse(auth_response)) {
-    console.debug(auth_response)
-    exit(-1);
-}
-
-console.debug(auth_response.data)
-
-
-const start = Date.now()
-const loop_control: LoopControl = {
-    credentials: credentials,
-    auth_info: auth_response.data,
-    start_time: start,
-    loop_time: start,
-    accumulate: [],
-    shouldContinue: true,
-    rate: rate,
-    offset: 0,
-    loop_fn: async function(loop_control: LoopControl) {
+    async loop_fn() {
         while(loop_control.shouldContinue) {
             for(const promise of Array(4).fill(undefined).map(() => {
-                const promise = fireGameRequest(loop_control.credentials, loop_control.rate, loop_control.offset)
+                const promise = fire_game_request(loop_control.credentials, loop_control.rate, loop_control.offset)
                 loop_control.offset += loop_control.rate
                 return promise
             })) {
                 const response = await promise
-                if(checkGameResponse(response)) {
-                    processData(loop_control.accumulate, response.data)
+                if(check_game_response(response)) {
+                    process_game_info_data(loop_control.accumulate, response.data)
                     console.debug(`Promise returned: ${response.status}`)
                     console.debug(response.data)
                 } else {
                     loop_control.shouldContinue = false
                     console.debug(response)
                 }
-
+    
             }
             console.debug(`Accumulated: ${loop_control.accumulate.length}`)
             const after_loop_time = Date.now()
             const expiry_time = Math.floor((after_loop_time - loop_control.start_time) / 1000)
             if(expiry_time >= loop_control.auth_info.expires_in) {
-                const auth_response = await fireAuthRequest(credentials)
+                const auth_response = await fire_auth_request(credentials)
         
-                if (!checkAuthResponse(auth_response)) {
+                if (!check_auth_response(auth_response)) {
                     console.debug(auth_response)
                     exit(-1);
                 }
@@ -102,24 +94,40 @@ const loop_control: LoopControl = {
             }
         
             const millis = after_loop_time - loop_control.loop_time
-
+    
             if (millis < 2500) {
                 await delay(2500 - millis)
             }
             loop_control.loop_time = after_loop_time
-
+    
             console.debug('Loop back.')
         }
-
     }
 }
 
-await loop_control.loop_fn(loop_control)
+
+
+
+const credentials: ClientCredentials = {
+    client_id: process.env.CLIENT_ID ?? '',
+    client_secret: process.env.CLIENT_SECRET ?? ''
+}
+
+const auth_response = await fire_auth_request(credentials)
+
+if (!check_auth_response(auth_response)) {
+    console.debug(auth_response)
+    exit(-1);
+}
+
+console.debug(auth_response.data)
+
+
+const loop_control = new LoopControl(credentials, auth_response.data)
+
+await loop_control.loop_fn()
 console.debug(loop_control.accumulate.join())
 
-function delay(millis) {
-    return new Promise(resolve => setTimeout(resolve, millis));
-}
 
 
 /*
@@ -139,33 +147,35 @@ if(Array.isArray(game_response.data)) {
 
 
 
+async function delay(millis: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, millis));
+}
 
-function checkSuccessCode(response: axios.Response<unknown>): boolean {
+function check_success_code(response: axios.Response<unknown>): boolean {
     return response.status === 200
 }
 
-function checkAuthResponse(auth_response: axios.Response<AuthTokenData>): boolean {
-    return checkSuccessCode(auth_response)
+function check_auth_response(auth_response: axios.Response<AuthTokenData>): boolean {
+    return check_success_code(auth_response)
 }
 
-function checkGameResponse(game_response: axios.Response<GameInfo[]>): boolean {
-    return checkSuccessCode(game_response) && game_response.data.length !== 0
+function check_game_response(game_response: axios.Response<GameInfo[]>): boolean {
+    return check_success_code(game_response) && game_response.data.length !== 0
 }
 
-function logGameResponseData(val: GameInfo) {
+function log_game_response_data(val: GameInfo) {
     console.debug(`Name: ${val.name}`);
     if(Array.isArray(val.alternative_names)) {
-            val.alternative_names?.forEach((val, i) => console.debug(`Alt ${i}: ${val.name}`))
+        val.alternative_names?.forEach((val, i) => console.debug(`Alt ${i}: ${val.name}`))
     }
 }
 
 
-async function fireAuthRequest(credentials: ClientCredentials): Promise<axios.Response<AuthTokenData>> {
+async function fire_auth_request(credentials: ClientCredentials): Promise<axios.Response<AuthTokenData>> {
     return redaxios.post(
     'https://id.twitch.tv/oauth2/token',
     '',
     {
-        method: 'post',
         params: new URLSearchParams({
             'client_id': credentials.client_id,
             'client_secret': credentials.client_secret,
@@ -177,7 +187,7 @@ async function fireAuthRequest(credentials: ClientCredentials): Promise<axios.Re
     }
 )};
 
-async function fireGameRequest(credentials: ClientCredentials, limit: number, offset: number): Promise<axios.Response<GameInfo[]>> {
+async function fire_game_request(credentials: ClientCredentials, limit: number, offset: number): Promise<axios.Response<GameInfo[]>> {
     return redaxios.post(
         "https://api.igdb.com/v4/games",
         `fields name, alternative_names.name; where name = "M"*; limit ${limit}; offset ${offset};`,
@@ -191,17 +201,21 @@ async function fireGameRequest(credentials: ClientCredentials, limit: number, of
     )
 }
 
-function processData(accumulate: string[], data: GameInfo[]) {
-    accumulate.push(...data.flatMap<string>(val => flattenGameInfo(val)))
+function process_game_info_data(accumulate: string[], data: GameInfo[]) {
+    accumulate.push(...data.flatMap<string>(val => flatten_game_info(val)))
 }
 
-function flattenGameInfo(val: GameInfo): string[] {
+function flatten_game_info(val: GameInfo): string[] {
     const accumulator: string[] = []
-    if(val.name.length < 10) {
+    if([...val.name].length === 6) {
         accumulator.push(val.name)
     } 
     if(Array.isArray(val.alternative_names)) {
-        accumulator.push(...val.alternative_names.map(val => val.name).filter(val => val.charAt(0) === "M" && val.length < 10))
+        accumulator.push(
+            ...val.alternative_names
+                .map(val => val.name)
+                .filter(val => val.charAt(0) === "M" && [...val].length === 6)
+        )
     }
     return accumulator
 }
